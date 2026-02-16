83456f023cf376b7b409ca06bb55c54f13db7aad
alexsandromercedes <alexsandromercedes@gmail.com>
2026-02-16 18:57:23 +0000

    Inicio da Função verificarMongoDB

--- inicio.py
+++ inicio.py
@@ -1,5 +1,25 @@
-#começo do projeto no github
-var_teste = 4+1
-teste = 5 + var_teste
-for i in range(teste):
-    print(i)
+# Idéia da solução
+def gambi(banco=None, user=None, tabela=None, valor=None):
+    try:
+        # Aqui seria a conexão com o banco de dados, usando as variáveis banco e user
+        # E depois a consulta na tabela usando a variável tabela e valor
+        print (f'Banco= {banco} Usuario= {user} Tabela= {tabela} Valor={valor}')
+        if banco is None:
+            raise ValueError("O parâmetro 'banco' é obrigatório.")
+        if user is None:
+            raise ValueError("O parâmetro 'user' é obrigatório.")
+        if tabela is None:
+            raise ValueError("O parâmetro 'tabela' é obrigatório.")
+        if valor is None:
+            raise ValueError("O parâmetro 'valor' é obrigatório.")
+
+        ...
+
+        return(True)
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+
+
+print (gambi(banco="mongodb", user="root", tabela="clientes", valor=10))
+
+
